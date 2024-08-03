1e8c93e8817fe2d224c505fb849a62de8f5a63f7
HamzaHersi01 <Hamza_Hersi01@hotmail.com>
2024-08-03 22:17:24 +0100

    added abilty to open and submit form using javascript

--- script.js
+++ script.js
@@ -1,10 +1,64 @@
-const myLibrary = [];
 
-function Book(title,author,pages) {
+const addBookbtn = document.querySelector(".addBookButton")
+const modalBackground = document.querySelector(".addBookModal")
+const form = document.querySelector("#bookinfo")
+console.log(form)
+
+const myLibrary = [];
+const modalToggled= false
+
+addBookbtn.addEventListener("click",function(){
+  document.querySelector('.addBookModal').classList.remove('hidden');
+}
+)
+modalBackground.addEventListener("click", function(event) {
+  if (event.target === modalBackground) {
+      modalBackground.classList.add('hidden');
+  }
+});
+
+form.addEventListener("submit",function(e){
+e.preventDefault();
+const title = form.title.value
+const author = form.author.value
+const pages = form.pages.value
+const read = form.read.value
+addBookToLibrary(new Book(title,author,pages,read))
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function Book(title,author,pages,read) {
   // the constructor...
   this.title = title
   this.author = author
   this.pages = pages
+  this.read = read
 }
 
 function addBookToLibrary(Book) {
