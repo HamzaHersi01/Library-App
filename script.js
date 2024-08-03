ac36745ba0a22472de9652e4cfd181b155db31b0
HamzaHersi01 <Hamza_Hersi01@hotmail.com>
2024-08-03 23:06:22 +0100

    added the ability to add books.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const addBookbtn = document.querySelector(".addBookButton")
 const modalBackground = document.querySelector(".addBookModal")
 const form = document.querySelector("#bookinfo")
+const content = document.querySelector(".content")
 console.log(form)
 
 const myLibrary = [];
@@ -24,12 +25,54 @@ const author = form.author.value
 const pages = form.pages.value
 const read = form.read.value
 addBookToLibrary(new Book(title,author,pages,read))
+populateTile(new Book(title,author,pages,read))
+modalBackground.classList.add('hidden');
 })
 
 
 
 
+function bookTileRender(){
+//var tiles = document.getElementsByClassName("bookTile")
+//while(tiles[0]){
+  //tiles[0].parentNode.removeChild(tiles[0])
+//}
 
+}
+
+function populateTile(book){
+  //TODO Title, Author, Pages, Read, Remove
+  const bookTile = document.createElement("div")
+  bookTile.classList.add("bookTile")
+
+  const title = document.createElement("div")
+  title.classList.add("title")
+  title.textContent = book.title
+  
+  const author = document.createElement("div")
+  author.classList.add("author")
+  author.textContent = book.author
+
+  const pages = document.createElement("div")
+  pages.classList.add("pages")
+  pages.textContent = book.pages
+
+  const read = document.createElement("div")
+  read.classList.add("read")
+  read.textContent = "Read"
+
+  const remove = document.createElement("div")
+  remove.classList.add("remove")
+  remove.textContent = "Remove"
+
+  bookTile.appendChild(title)
+  bookTile.appendChild(author)
+  bookTile.appendChild(pages)
+  bookTile.appendChild(read)
+  bookTile.appendChild(remove)
+
+  content.appendChild(bookTile)
+}
 
 
 
